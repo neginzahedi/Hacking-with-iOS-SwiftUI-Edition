d5738132235dc69690748b62a79cd011f1011c26
Negin Zahedi <zahedi.negin@gmail.com>
2024-10-03 17:30:47 -0400

    Create new project named WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Negin Zahedi on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
