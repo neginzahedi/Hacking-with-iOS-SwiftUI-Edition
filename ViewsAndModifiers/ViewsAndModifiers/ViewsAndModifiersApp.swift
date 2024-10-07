e5e7cc56532cc5cabba69cbf417b1396b4eb38ce
Negin Zahedi <zahedi.negin@gmail.com>
2024-10-07 16:23:05 -0400

    Create "Views and Modifiers" project

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Negin Zahedi on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
