97391845f0d908e39d36e9120f168ac2300251ae
Negin Zahedi <zahedi.negin@gmail.com>
2024-10-07 13:01:15 -0400

    Create GuessTheFlag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Negin Zahedi on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
