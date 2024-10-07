dc748e7d7ba0dc41251240e9e8de0896c41dd972
Negin Zahedi <zahedi.negin@gmail.com>
2024-10-07 17:01:06 -0400

    Create "BetterRest" project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Negin Zahedi on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
