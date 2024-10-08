6704450d2b36d1e6d61e327be036168d16b5664d
Negin Zahedi <zahedi.negin@gmail.com>
2024-10-08 19:01:43 -0400

    Complete "iExpense" challenge

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Negin Zahedi on 2024-10-08.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
