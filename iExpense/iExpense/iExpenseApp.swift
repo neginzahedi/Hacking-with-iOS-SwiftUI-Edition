10cc4ee854ca7e213f14f6e71ead383461d88536
Negin Zahedi <zahedi.negin@gmail.com>
2024-10-08 16:22:09 -0400

    Create "iExpense" project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Negin Zahedi on 2024-10-08.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
